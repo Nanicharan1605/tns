5a10c3a11d67aa5101287e2665cd8ca985676726
Vivek Sai <Vivek Sai@Vivek>
2025-10-12 18:12:40 +0530

    streamapi

---
+++ stream_api/src/stream_api/Product.java
@@ -0,0 +1,17 @@
+package stream_api;
+//demo for stream api 
+
+public class Product {
+	int id;
+	String name;
+	float price;
+	
+	// parameter constructor 
+	
+	public Product(int id, String name, float price) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.price = price;
+	}
+}
